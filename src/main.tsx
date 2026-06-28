be7ec8cc51d413cae31620e9900648216969d8a0
Jon Mico <jonmico@gmail.com>
2026-06-28 12:56:09 -0400

    Adds some style pages.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router";
+import "./styles/reset.css";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
