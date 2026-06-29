1e17b7be2808e5950384a89021d1a31a4799c2da
Jon Mico <jonmico@gmail.com>
2026-06-28 22:23:23 -0400

    Adds some state to register form.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import styles from "./Register.module.css";
 
+interface FormState {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const initialFormState: FormState = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 export default function Register() {
+  const [formState, setFormState] = useState<FormState>(initialFormState);
+
+  console.log(formState);
+
+  function handleFormStateChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    setFormState();
+  }
   return (
     <div className={styles.registerPageContainer}>
       <div className={styles.registerFormContainer}>
         <h1>Register</h1>
         <form className={styles.registerForm}>
-          <input type="text" placeholder="something" />
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              id="email"
+              type="email"
+              placeholder="something"
+              onChange={handleFormStateChange}
+            />
+          </div>
           <input type="text" placeholder="something" />
           <input type="text" placeholder="something" />
           <button>Click me :)</button>
