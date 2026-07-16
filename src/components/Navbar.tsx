a2f609b57d89aa77b0bfe7d634b4297f080996de
Jon Mico <jonmico@gmail.com>
2026-07-15 20:33:14 -0400

    - Starts building dropdown menu for user icon in Navbar. - Adds conditional rendering to Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 import { Link, NavLink } from "react-router";
 import styles from "./Navbar.module.css";
+import { useAuth } from "../hooks/useAuth";
+import { CircleUserRound } from "lucide-react";
+import { useState } from "react";
 
 export default function Navbar() {
+  const { isLoggedIn, firstName } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <nav className={styles.navbar}>
       <div>
@@ -10,13 +20,26 @@ export default function Navbar() {
         </Link>
       </div>
       <div className={styles.auth}>
-        <NavLink className={styles.link} to="/login">
-          Login
-        </NavLink>
-        <NavLink className={styles.link} to="/register">
-          Register
-        </NavLink>
+        {!isLoggedIn ? (
+          <>
+            <NavLink className={styles.link} to="/login">
+              Login
+            </NavLink>
+            <NavLink className={styles.link} to="/register">
+              Register
+            </NavLink>
+          </>
+        ) : (
+          <div className={styles.user} onClick={handleClick}>
+            <CircleUserRound />
+            {isOpen && <DropDown />}
+          </div>
+        )}
       </div>
     </nav>
   );
+}
+
+function DropDown() {
+  return <div onClick={(evt) => evt.stopPropagation()}></div>;
 }
