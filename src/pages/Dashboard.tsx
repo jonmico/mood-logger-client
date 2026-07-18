7084e9d399badc77287fcfeeec6f36518ae51a89
Jon Mico <jonmico@gmail.com>
2026-07-18 08:55:10 -0400

    Adds TODO to Dashboard.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { useAuth } from "../hooks/useAuth";
 
+// TODO: This is for a test commit. Also I need to figure out what to put on this page.
+
 export default function Dashboard() {
   const { firstName } = useAuth();
   return <div>Hello, {firstName}! Welcome to Mood Logger!</div>;
